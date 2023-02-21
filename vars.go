8338aafadb2a6c4495c768f025f30eafda1c5906
Eduardo Flores <eduardofv@gmail.com>
2023-02-21 14:17:11 -0600

    variables

---
+++ vars.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "init"
+	fmt.Println("Init: ", a)
+
+	b := 10
+	fmt.Println("Init: ", b)
+
+	var i int
+	var s string
+
+	fmt.Println("i:", i)
+	fmt.Println("s:", s)
+}
