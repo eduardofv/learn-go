fcf09b654e23d127a2c8dbe2ab6b825605e7963c
Eduardo Flores <eduardofv@gmail.com>
2023-02-25 18:06:40 -0600

    added boyer-moore

--- most_freq.go
+++ most_freq.go
@@ -32,12 +32,36 @@ func most_frequent(arr []int) int {
 	return most_freq_value
 }
 
+//https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+func boyer_moore(arr []int) int {
+	var most_freq_votes, most_freq_value int;
+
+	for _, value := range arr {
+		if most_freq_votes == 0 {
+			most_freq_votes = 1
+			most_freq_value = value
+		} else if value == most_freq_value {
+			most_freq_votes += 1
+		} else {
+			most_freq_votes -= 1
+		}
+	}
+
+	fmt.Println("votes: ", most_freq_votes)
+	return most_freq_value
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	arr := gen_array(10, 7)
+	arr := gen_array(10, 6)
+	//arr := []int{0, 1, 1, 1, 2, 3}
 
 	fmt.Println("arr: ", arr)
 	mfv := most_frequent(arr)
 	fmt.Println("most_freq: ", mfv)
+
+	fmt.Println("---")
+	mfv = boyer_moore(arr)
+	fmt.Println("boyer/moore most_freq: ", mfv)
 }
