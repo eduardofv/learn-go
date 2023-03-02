b5275238f098944f85a0bf4091a7469cb319d23b
Eduardo Flores <eduardofv@gmail.com>
2023-03-01 18:15:05 -0600

    goroutines

---
+++ gorut.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var gi = 1
+
+func f(from string) {
+	gi += 1 
+	for i := 0; i < 3; i++ {
+		time.Sleep(time.Duration(gi) * time.Second)
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	go f("goroutine1")
+	go f("goroutine2")
+
+
+	time.Sleep(time.Duration(10) * time.Second)
+}
