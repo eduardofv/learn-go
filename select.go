450739b7a97cd46469a54c826c1526324395cc84
Eduardo Flores <eduardofv@gmail.com>
2023-03-03 12:11:01 -0600

    timeout

--- select.go
+++ select.go
@@ -21,12 +21,19 @@ func main() {
 		c1 <- "two"
 	}()
 
-	for i := 0; i < 2; i++ {
+	go func() {
+		c1 <- "three"
+		time.Sleep(10 * time.Second)
+	}()
+
+	for i := 0; i < 5; i++ {
 		select {
 		case msg1 := <-c1:
 			fmt.Println("received", msg1)
 		case msg2 := <-c2:
 			fmt.Println("received", msg2)
+		case <-time.After(3 * time.Second):
+			fmt.Println("timeout")
 		}
 
 	}
